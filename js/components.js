2aa251c67576504b57f1d503611406dc7db91dd2
lb-saroj <saroj.dhwaz@study.lbef.edu.np>
2026-01-11 04:14:15 +0000

    Add base implementation for gh-pages and update navigation links in Header component

--- js/components.js
+++ js/components.js
@@ -1,17 +1,24 @@
-// js/components.js]
+// js/components.js
 // Header Component
 export class Header extends HTMLElement {
   connectedCallback() {
+    // Add base implementation for gh-pages
+    if (!document.querySelector('base')) {
+      const base = document.createElement('base');
+      base.href = '/CT053-3-1-FDD-Assignment/';
+      document.head.appendChild(base);
+    }
+
     this.innerHTML = `
         <header>
             <nav>
                 <div class="logo">
-                    <a href="/index.html" style="color:white; text-decoration:none;">
+                    <a href="/" style="color:white; text-decoration:none;">
                         <h1><i class="fas fa-globe-asia"></i> LBEF Global</h1>
                     </a>
                 </div>
                 <ul class="nav-links">
-                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="/pages/general/about.html">About</a></li>
                     <li><a href="/pages/events/home.html">Events</a></li>
                     <li><a href="/pages/academic/partnership.html">Academics</a></li>
