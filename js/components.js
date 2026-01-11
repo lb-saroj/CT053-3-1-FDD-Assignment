85c192060cfc1218b00bc04ff73e6e137cc70867
lb-saroj <saroj.dhwaz@study.lbef.edu.np>
2026-01-11 02:30:52 +0000

    Replace header and footer sections with custom elements in multiple HTML files

--- js/components.js
+++ js/components.js
@@ -6,16 +6,16 @@ export class Header extends HTMLElement {
         <header>
             <nav>
                 <div class="logo">
-                    <a href="./index.html" style="color:white; text-decoration:none;">
+                    <a href="/index.html" style="color:white; text-decoration:none;">
                         <h1><i class="fas fa-globe-asia"></i> LBEF Global</h1>
                     </a>
                 </div>
                 <ul class="nav-links">
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./pages/general/about.html">About</a></li>
-                    <li><a href="./pages/events/home.html">Events</a></li>
-                    <li><a href="./pages/academic/partnership.html">Academics</a></li>
-                    <li><a href="./pages/membership/contact.html">Contact</a></li>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/pages/general/about.html">About</a></li>
+                    <li><a href="/pages/events/home.html">Events</a></li>
+                    <li><a href="/pages/academic/partnership.html">Academics</a></li>
+                    <li><a href="/pages/membership/contact.html">Contact</a></li>
                 </ul>
             </nav>
         </header>
